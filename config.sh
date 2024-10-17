434ec857a15c6277cc63531d6f5c717459858a79
AndreyDogadin <andrey.dogadin@protonmail.com>
2024-10-17 19:01:47 +0200

    params extracted into separate file

--- config.sh
+++ config.sh
@@ -1,1 +1,9 @@
- 
+#!/bin/sh
+
+# Configuration parameters
+HOSTS="192.168.0.1 example.com 3.3.4.4"
+URLS="http://192.168.0.5:8080/owncloud http://example.com http://notexist5.com https://google.com https://shmugl.au"
+COUNT=6
+TELEGRAM_API="https://api.telegram.org/botXXXXXXXXXX:AAAAAAAAA-BBBBBBBBBBBBBBBBBBBBBBBBB"
+TELEGRAM_CHAT_ID=-511111111
+MONITORING_DIR="/tmp/.monitoring"
